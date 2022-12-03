be3f8ebb665ba9a2fd7d09e4316ed7fdb5a625e1
JacobSeo <jacob@changtalk.kr>
2022-12-04 00:49:52 +0900

    백업

--- pybo/models.py
+++ pybo/models.py
@@ -5,6 +5,8 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,4 +15,12 @@ class Answer(db.Model):
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
 
-#SQLAlchemy가 결국엔 Django에서 SubQuery느낌으로 사용됨.
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+# SQLAlchemy가 결국엔 Django에서 SubQuery느낌으로 사용됨.
+# flask db migrate
+# flask db upgrade
